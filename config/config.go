23375cb80f4a13d6050466bf0e405bf82cfca7e1
tyspice <tyner27@gmail.com>
2023-08-19 17:17:16 -0700

    move config into its own package

---
+++ config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Port string `yaml:"port" envconfig:"SERVER_PORT"`
+		Secret string `yaml:"secret" envconfig:"SECRET"`
+	} `yaml:"server"`
+	Database struct {
+		Username string `yaml:"username" envconfig:"DB_USERNAME"`
+		Password string `yaml:"password" envconfig:"DB_PASSWORD"`
+		Host     string `yaml:"host" envconfig:"DB_HOST"`
+		Port     string `yaml:"port" envconfig:"DB_PORT"`
+	} `yaml:"database"`
+}
+
+var Cfg Config
+
+func InitConfig() error {
+	f, err := os.Open("local.yaml")
+	if err == nil {
+		decoder := yaml.NewDecoder(f)
+		err = decoder.Decode(&Cfg)
+		if err != nil {
+			return err
+		}
+	} else {
+		// We assume that the error was caused by a missing
+		// local file which isn't fatal because it most likely means
+		// we'll fall back to an env variable
+		log.Println(err.Error())
+	}
+	f.Close()
+
+	err = envconfig.Process("", &Cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -2,54 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/kelseyhightower/envconfig"
+	"github.com/tyspice/mr-grocery-sql/config"
 	"github.com/tyspice/mr-grocery-sql/models"
 	"github.com/tyspice/mr-grocery-sql/router"
-	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
-	Server struct {
-		Port string `yaml:"port" envconfig:"SERVER_PORT"`
-	} `yaml:"server"`
-	Database struct {
-		Username string `yaml:"username" envconfig:"DB_USERNAME"`
-		Password string `yaml:"password" envconfig:"DB_PASSWORD"`
-		Host     string `yaml:"host" envconfig:"DB_HOST"`
-		Port     string `yaml:"port" envconfig:"DB_PORT"`
-	} `yaml:"database"`
-}
-
-func initConfig() Config {
-	var cfg Config
-	f, err := os.Open("local.yaml")
-	if err == nil {
-		decoder := yaml.NewDecoder(f)
-		err = decoder.Decode(&cfg)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		// We assume that the error was caused by a missing
-		// local file which isn't fatal because it most likely means
-		// we'll fall back to an env variable
-		log.Println(err.Error())
-	}
-	f.Close()
-
-	err = envconfig.Process("", &cfg)
-	if err != nil {
-		panic(err)
-	}
-	return cfg
-}
-
 func main() {
-	config := initConfig()
-	err := models.InitDB(config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port)
+	err := config.InitConfig()
+	if (err != nil) {
+		panic(err)
+	} else {
+		fmt.Println("Configuration successfully initialized")
+	}
+	err = models.InitDB(config.Cfg.Database.Username, config.Cfg.Database.Password, config.Cfg.Database.Host, config.Cfg.Database.Port)
 	if err != nil {
 		panic(err)
 	} else {
@@ -58,5 +24,5 @@ func main() {
 	defer models.Finished()
 
 	r := router.InitRouter()
-	r.Run(":" + config.Server.Port)
+	r.Run(":" + config.Cfg.Server.Port)
 }
