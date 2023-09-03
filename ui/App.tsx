fbf9384197ffe6e9d3f1448005d5f033150b92ab
tyspice <tyner27@gmail.com>
2023-09-02 18:06:42 -0700

    add theme library and basic header

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet } from "react-native";
+import { createTheme, ThemeProvider, darkColors, Header } from "@rneui/themed";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const theme = createTheme({
+  darkColors: {
+    ...Platform.select({
+      default: darkColors.platform.ios,
+      ios: darkColors.platform.ios,
+    }),
+  },
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider
+        style={{ backgroundColor: theme.darkColors?.background }}
+      >
+        <Header
+          centerComponent={{ text: "Mr Grocery", style: styles.heading }}
+        ></Header>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  heading: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
