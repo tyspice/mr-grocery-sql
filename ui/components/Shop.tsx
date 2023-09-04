22e1e4e96790d06e97f52260e4dda3da23da3e30
tyspice <tyner27@gmail.com>
2023-09-03 23:08:52 -0700

    add list itemsd to shoppong section

--- ui/components/Shop.tsx
+++ ui/components/Shop.tsx
@@ -1,7 +1,33 @@
-import { YStack, Button } from "tamagui";
+import { YStack, ListItem, YGroup } from "tamagui";
 
 export const Shop = () => (
-  <YStack f={1} jc="center" ai="center" backgroundColor={"$background"}>
-    <Button theme="blue">Shop</Button>
+  <YStack f={1} jc="space-around" ai="center" backgroundColor={"$background"}>
+    <YGroup>
+      {["carrots", "snails", "smut", "snake"].map((e, i) => (
+        <YGroup.Item key={i}>
+          <ListItem
+            theme={i % 2 === 0 ? "blue" : "blue_alt1"}
+            size="$8"
+            key={i}
+          >
+            {e}
+          </ListItem>
+        </YGroup.Item>
+      ))}
+    </YGroup>
+
+    <YGroup>
+      {["rats", "football", "turkey"].map((e, i) => (
+        <YGroup.Item key={i}>
+          <ListItem
+            theme={i % 2 === 0 ? "green" : "green_alt1"}
+            size="$8"
+            key={i}
+          >
+            {e}
+          </ListItem>
+        </YGroup.Item>
+      ))}
+    </YGroup>
   </YStack>
 );
