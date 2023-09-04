3cd573dfea9b02c9b7413bb24728d927740ad740
tyspice <tyner27@gmail.com>
2023-09-03 21:20:13 -0700

    add bottom tabs

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,18 +1,24 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { navigationRef } from "./RootNavigation";
 
 import { useFonts } from "expo-font";
-import { TamaguiProvider, Theme, YStack, Button } from "tamagui";
+import { TamaguiProvider, Theme, YStack, Button, Image } from "tamagui";
 
 import config from "./tamagui.config";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const Home = () => (
   <YStack f={1} jc="center" ai="center" backgroundColor={"$background"}>
-    <Button theme="blue">Hello Tyner</Button>
+    <Button theme="blue">Home</Button>
+  </YStack>
+);
+
+const Settings = () => (
+  <YStack f={1} jc="center" ai="center" backgroundColor={"$background"}>
+    <Button theme="blue">Settings</Button>
   </YStack>
 );
 
@@ -30,16 +36,20 @@ export default function App() {
       <SafeAreaProvider>
         <TamaguiProvider config={config}>
           <Theme name="dark">
-            <Stack.Navigator
+            <Tab.Navigator
               screenOptions={{
                 headerStyle: {
+                  backgroundColor: "#000",
+                },
+                tabBarStyle: {
                   backgroundColor: "#000",
                 },
                 headerTintColor: "#fff",
               }}
             >
-              <Stack.Screen name="Home" component={Home}></Stack.Screen>
-            </Stack.Navigator>
+              <Tab.Screen name="Home" component={Home}></Tab.Screen>
+              <Tab.Screen name="Settings" component={Settings}></Tab.Screen>
+            </Tab.Navigator>
           </Theme>
         </TamaguiProvider>
       </SafeAreaProvider>
