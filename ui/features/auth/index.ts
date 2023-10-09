781cef9e3909270823f8fd769460be977b7891fe
tyspice <tyner27@gmail.com>
2023-10-09 13:32:13 -0700

    add axios and async thunk for loading token

--- ui/features/auth/index.ts
+++ ui/features/auth/index.ts
@@ -1,4 +1,19 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// TODO: don't pass username and password via payload.
+const getToken = createAsyncThunk(
+  "auth/getToken",
+  async (credentials: { username: string; password: string }, thunkAPI) => {
+    const token = await axios.get("localhost:3000/token", {
+      data: {
+        email: credentials.username,
+        password: credentials.password,
+      },
+    });
+    return token;
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
