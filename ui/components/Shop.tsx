a65432d91d336cfcc5f5c9d848f238bb84c63463
tyspice <tyner27@gmail.com>
2023-10-22 17:02:03 -0700

    add sheet

--- ui/components/Shop.tsx
+++ ui/components/Shop.tsx
@@ -1,13 +1,26 @@
-import { YStack, ListItem, YGroup, ScrollView, Checkbox } from "tamagui";
+import {
+  YStack,
+  ListItem,
+  YGroup,
+  ScrollView,
+  Checkbox,
+  Button,
+  Sheet,
+  Input,
+} from "tamagui";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getShoppingItems, updateItem } from "../features/items";
-import { Check, ShoppingCart, X } from "@tamagui/lucide-icons";
+import { Check, ChevronDown, ChevronUp } from "@tamagui/lucide-icons";
 import { Item } from "../models";
 import _ from "lodash-es";
 
 export const Shop = () => {
+  const [open, setOpen] = useState(false);
+  const [position, setPosition] = useState(0);
+  const [innerOpen, setInnerOpen] = useState(false);
+
   const dispatch = useDispatch<AppDispatch>();
   const { shoppingItems } = useSelector((state: RootState) => state.items);
   useEffect(() => {
@@ -20,13 +33,8 @@ export const Shop = () => {
   };
 
   return (
-    <ScrollView backgroundColor={"$background"}>
-      <YStack
-        f={1}
-        jc="space-around"
-        ai="center"
-        backgroundColor={"$background"}
-      >
+    <YStack backgroundColor={"$background"} fullscreen={true}>
+      <ScrollView backgroundColor={"$background"}>
         <YGroup space>
           {shoppingItems.map((e, i) => (
             <YGroup.Item key={i}>
@@ -55,7 +63,44 @@ export const Shop = () => {
             </YGroup.Item>
           ))}
         </YGroup>
-      </YStack>
-    </ScrollView>
+      </ScrollView>
+      <Button theme={"blue"} onPress={() => setOpen(true)}>
+        <ChevronUp />
+      </Button>
+
+      <Sheet
+        forceRemoveScrollEnabled={open}
+        modal={false}
+        open={open}
+        onOpenChange={setOpen}
+        dismissOnSnapToBottom
+        position={position}
+        onPositionChange={setPosition}
+        zIndex={100_000}
+        animation="bouncy"
+      >
+        <Sheet.Overlay
+          animation="lazy"
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+        />
+        <Sheet.Handle />
+        <Sheet.Frame
+          padding="$4"
+          justifyContent="flex-start"
+          alignItems="center"
+          theme={"blue"}
+          space="$5"
+        >
+          <Button
+            size="$6"
+            circular
+            icon={ChevronDown}
+            onPress={() => setOpen(false)}
+          />
+          <Input width={200} />
+        </Sheet.Frame>
+      </Sheet>
+    </YStack>
   );
 };
